cb119a60598e8cd143fff3b1a562900578185e82
matt2393 <mattct2393@gmail.com>
2018-10-19 16:38:42 -0400

    formulario evaluacion de reuniones

--- cliente/src/app/shared/evaluacion-reuniones/evaluacion-reuniones.component.ts
+++ cliente/src/app/shared/evaluacion-reuniones/evaluacion-reuniones.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, FormControl, Validators} from '@angular/forms';
+import {AuthService} from '../../auth.service';
+import {NbToastrService} from '@nebular/theme';
+import {EmpresaService} from '../../admin/empresa/empresa.service';
 
 @Component({
   selector: 'ngx-evaluacion-reuniones',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluacionReunionesComponent implements OnInit {
 
-  constructor() { }
+    empresas: any = null;
+    medio_comunicacion: any = null;
+    evaluacionGroup: FormGroup;
 
-  ngOnInit() {
-  }
+    empresa_id = 0;
+    registrado = false;
+    constructor(private fb: FormBuilder,
+                private toastr: NbToastrService,
+                private authService: AuthService,
+                private empresaService: EmpresaService) {
+        this.empresa_id = this.authService.getUsuario().empresa_id;
+        this.empresaService.empresasListar()
+            .subscribe((res: any[]) => {
+                this.empresas = res;
+            });
+        this.createForm();
+    }
 
+    createForm() {
+        this.evaluacionGroup = this.fb.group({
+            'empresa_id': this.authService.getUsuario().empresa_id,
+            'uno': new FormControl('', Validators.required),
+            'dos': new FormControl('', Validators.required),
+            'tres': new FormControl('', Validators.required),
+            'cuatro': new FormControl('', Validators.required),
+            'cinco': new FormControl('', Validators.required),
+            'seis': new FormControl('', Validators.required),
+            'siete': new FormControl('', Validators.required),
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    store() {
+
+    }
 }
