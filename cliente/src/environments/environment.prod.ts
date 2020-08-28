aef81aa6f95fe51572613a943d76647d6111a8c7
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2020-08-28 00:56:04 -0400

    rueda de bioseguridad 28082020 0056

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   baseY: 'http://gotasoft.com/sistemas/rueda/servidor/public/api/',
   baseZ: 'http://servidor-bioseguridad.campoferial3jvirtual.com/api/',
-  base: 'http://servidor-fimem.campoferial3jvirtual.com/api/',
+  base: 'http://servidor-bioseguridad.campoferial3jvirtual.com/api/',
   baseX: 'http://localhost:8000/api/',
   base2: 'http://rueda.campoferial3dejulio.com/public/api/',
   base3: 'http://3.233.38.53/rueda/servidor/public/api/',
